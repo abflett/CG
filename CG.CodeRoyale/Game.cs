6b0df2925582ae4aa3c5700c693c58216da45f97
Adam Flett <adam@adamflett.net>
2023-12-18 22:54:21 +0800

    Add project files.

---
+++ CG.CodeRoyale/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Game
+{
+    private int[] _inputs = null;
+
+    public void Run()
+    {
+        Player player = new();
+
+        GameSites gameSites = new(Util.GetNumericValue());
+        for (int i = 0; i < gameSites.NumberOfSites; i++)
+        {
+            _inputs = Util.GetNumericValues();
+            gameSites.AddSite(_inputs[0], _inputs[1], _inputs[2], _inputs[3]);
+        }
+
+        while (true)
+        {
+            _inputs = Util.GetNumericValues();
+
+            player.UpdateInfo(_inputs[0], _inputs[1]);
+            for (int i = 0; i < gameSites.NumberOfSites; i++)
+            {
+                _inputs = Util.GetNumericValues();
+                gameSites.UpdateSiteInfo(_inputs[0], _inputs[1], _inputs[2], _inputs[3], _inputs[4], _inputs[5], _inputs[6]);
+            }
+
+            GameUnits gameUnits = new(Util.GetNumericValue());
+            for (int i = 0; i < gameUnits.NumberOfUnits; i++)
+            {
+                _inputs = Util.GetNumericValues();
+                gameUnits.AddUnit(_inputs[0], _inputs[1], _inputs[2], _inputs[3], _inputs[4]);
+            }
+
+            player.Update(gameSites, gameUnits);
+
+            Console.WriteLine(player.QueenAction);
+            Console.WriteLine(player.TrainAction);
+        }
+    }
+}
---
+++ CG.CodeRoyale/GameSites.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+class GameSites
+{
+    public int NumberOfSites { get; set; }
+    public List<Site> Sites { get; set; } = new List<Site>();
+
+    public GameSites(int numberOfSites)
+    {
+        NumberOfSites = numberOfSites;
+    }
+
+    public void AddSite(int siteId, int x, int y, int radius)
+    {
+        Sites.Add(new Site
+        {
+            Id = siteId,
+            X = x,
+            Y = y,
+            Radius = radius
+        });
+    }
+
+    public void UpdateSiteInfo(int siteId, int ignore1, int ignore2, int structureType, int owner, int param1, int param2)
+    {
+        Site site = Sites.Find(x => x.Id == siteId);
+        site.UpdateSiteInfo(ignore1, ignore2, structureType, owner, param1, param2);
+    }
+}
